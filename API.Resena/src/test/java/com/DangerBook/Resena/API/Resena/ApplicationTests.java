4fe45818fac6f28c732a6194a7cce6a08849e1b2
floatinggxx <st.lazaro@duocuc.cl>
2025-09-14 22:39:18 -0300

    Add files via upload

---
+++ API.Resena/src/test/java/com/DangerBook/Resena/API/Resena/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DangerBook.Resena.API.Resena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
